bd8a8544957a31703b4b0cd4cc08688028b444f0
Fatema-Nawrin <nawrin4000@gmail.com>
2022-05-08 22:03:52 +0600

    cors update

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 
-// middleware 
-app.use(cors());
+// middleware
+const corsConfig = {
+    origin: true,
+    credentials: true,
+}
+app.use(cors(corsConfig));
+app.options('*', cors(corsConfig))
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h84nk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
